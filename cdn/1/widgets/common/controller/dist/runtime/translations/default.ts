995c638ad0a9b5baf4ccabd2ea453f67334231d1
sukruburakcetin <cetin.sukruburak@gmail.com>
2026-02-26 15:30:06 +0300

    clearance before updating

--- cdn/1/widgets/common/controller/dist/runtime/translations/default.ts
+++
@@ -1,7 +0,0 @@
-export default {
-  _widgetLabel: 'Widget Controller',
-  _action_openWidget_label: 'Open widget',
-  _action_toggleWidget_label: 'Toggle widget',
-  resizerTooltip: 'Customize the window size by dragging and dropping.',
-  moreWidgets: 'More widgets'
-}
